d3349fd38a3f72c01d3a8bb0e993879da72ff7f4
SurajGonjari <suraj.gonjari@infogain.com>
2024-05-18 12:57:23 +0530

    Basecode Commit

---
+++ src/main/java/com/vaadin/starter/bakery/app/security/SecurityConfiguration.java
@@ -0,0 +1,73 @@
+package com.vaadin.starter.bakery.app.security;
+
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import com.vaadin.starter.bakery.backend.data.entity.User;
+import com.vaadin.starter.bakery.backend.repositories.UserRepository;
+import com.vaadin.starter.bakery.ui.views.login.LoginView;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+/**
+ * Configures spring security, doing the following:
+ * <li>Bypass security checks for static resources,</li>
+ * <li>Restrict access to the application, allowing only logged in users,</li>
+ * <li>Set up the login form,</li>
+ * <li>Configures the {@link UserDetailsServiceImpl}.</li>
+ * 
+ */
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends VaadinWebSecurity {
+
+	/**
+	 * The password encoder to use when encrypting passwords.
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public CurrentUser currentUser(UserRepository userRepository) {
+		final String username = SecurityUtils.getUsername();
+		User user = username != null ? userRepository.findByEmailIgnoreCase(username) : null;
+		return () -> user;
+	}	
+
+	/**
+	 * Require login to access internal pages and configure login form.
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		setLoginView(http, LoginView.class);
+	}
+
+	/**
+	 * Allows access to static resources, bypassing Spring security.
+	 * 
+	 * @throws Exception
+	 */
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		super.configure(web);
+		web.ignoring().requestMatchers(
+				// the robots exclusion standard
+				new AntPathRequestMatcher("/robots.txt"),
+				// icons and images
+				new AntPathRequestMatcher("/icons/**"),
+				new AntPathRequestMatcher("/images/**"),
+				// (development mode) H2 debugging console
+				new AntPathRequestMatcher("/h2-console/**"));
+	}
+}
